8c68b9b187c229b92519f71e1fb83ecea0854047
Garlic Fiend <suzannaakins@gmail.com>
2020-09-01 00:09:45 -0700

    button colors and other UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -157,7 +157,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -193,6 +193,20 @@ $(".card .list-group").sortable({
   scroll: false,
   tolerance: "pointer",
   helper: "clone",
+  activate: function (event) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
+  },
+  deactivate: function (event) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
+  },
+  over: function (event) {
+    $(event.target).addClass("dropover-active");
+  },
+  out: function (event) {
+    $(event.target).removeClass("dropover-active");
+  },
   //update tasks to local storage
   update: function (event) {
     //array to store task data
@@ -231,13 +245,14 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function (event, ui) {
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
-  // over: function (event, ui) {
-  //   console.log("over");
-  // },
-  // out: function (event, ui) {
-  //   console.log("out");
-  // }
+  over: function (event, ui) {
+    $(".bottom-trash").addClass("bottom-trash-active");
+  },
+  out: function (event, ui) {
+    $(".bottom-trash").removeClass("bottom-trash-active");
+  }
 });
 
 //color code tasks that are overdue or close to due
@@ -260,9 +275,9 @@ var auditTask = function (taskEl) {
 // load tasks for the first time
 loadTasks();
 
-setInterval(function() {
+setInterval(function () {
   $(".card .list-group-item").each(function (el) {
     auditTask(el);
-    });
+  });
 }, 1800000);
 
